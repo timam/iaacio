292c72a3a63d18d0d2623e35d87b78bd472a95fc
Timam <me@timam.io>
2018-08-14 00:14:08 +0600

    mini rds added

--- prod/data_store/main.tf
+++ prod/data_store/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-# Collecting "mavs-vpc" ID
+# Collecting VPC ID
 data "aws_vpc" "mavs-vpc" {
   filter {
     name = "tag:Name"
@@ -11,85 +11,58 @@ data "aws_vpc" "mavs-vpc" {
   }
 }
 
+# Define DB Security Groupgit
+resource "aws_security_group" "swin-rds-sg" {
+  name = "swin-rds-sg"
 
-resource "aws_db_security_group" "default" {
-  name = "rds_sg"
+  description = "RDS postgres servers (terraform-managed)"
+  vpc_id = "${data.aws_vpc.mavs-vpc.id}"
 
+  # Allow only mysql within vpc only
   ingress {
-    cidr = "10.0.0.0/24"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-module "db" {
-  source = "../../modules/rds"
+# Define swin-rds
+resource "aws_db_instance" "swin-rds" {
+  allocated_storage        = 10
+  backup_retention_period  = 7
+  db_subnet_group_name     = "db-subnet"
 
-  identifier = "demodb"
+  engine                   = "mysql"
+  engine_version           = "5.7"
+  instance_class           = "db.t2.micro"
+  multi_az                 = false
+  port                     = 3306
+  identifier               = "swin-rds"
+  name                     = "swindb"
 
-  engine            = "mysql"
-  engine_version    = "5.7.19"
-  instance_class    = "db.t2.small"
-  allocated_storage = 5
+  username                 = "foo"
+  password                 = "foobarbaz"
+  parameter_group_name     = "default.mysql5.7"
+  publicly_accessible      = false
 
-  name     = "demodb"
-  username = "user"
-  password = "YourPwdShouldBeLongAndSecure!"
-  port     = "3306"
+  storage_encrypted        = false
+  storage_type             = "gp2"
+  vpc_security_group_ids   = ["${aws_security_group.swin-rds-sg.id}"]
 
-  iam_database_authentication_enabled = true
-
-  vpc_security_group_ids = ["${aws_db_security_group.default.id}"]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval = "30"
-  monitoring_role_name = "MyRDSMonitoringRole"
-  create_monitoring_role = true
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  # DB subnet group
-  subnet_ids = ["${data.aws_subnet.db_subnet.id}"]
-
-  # DB parameter group
-  family = "mysql5.7"
-
-  # DB option group
-  major_engine_version = "5.7"
-
-  # Snapshot name upon DB deletion
-  final_snapshot_identifier = "demodb"
-
-  parameters = [
-    {
-      name = "character_set_client"
-      value = "utf8"
-    },
-    {
-      name = "character_set_server"
-      value = "utf8"
-    }
-  ]
-
-  options = [
-    {
-      option_name = "MARIADB_AUDIT_PLUGIN"
-
-      option_settings = [
-        {
-          name  = "SERVER_AUDIT_EVENTS"
-          value = "CONNECT"
-        },
-        {
-          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-          value = "37"
-        },
-      ]
-    },
-  ]
+  final_snapshot_identifier = "final-swin-snapshot"
 }
+
+
+######################################################################################################
+## db_subnet_group_name     = "db-subnet#                                                           ##
+## password                 = "${trimspace(file("${path.module}/secrets/mydb1-password.txt"))}"     ##
+######################################################################################################
