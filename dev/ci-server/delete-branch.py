555939362951ee365adf2d8b3fcbc9c6993056ee
Timam <me@timam.io>
2018-08-21 11:29:59 +0600

    deleting dirictory & confs

--- dev/ci-server/delete-branch.py
+++ dev/ci-server/delete-branch.py
@@ -5,9 +5,14 @@ branch_name = raw_input("please enter the branch name you want to delete: ")
 sites_available_file = "/etc/nginx/sites-available/swin-"+branch_name+"devam.pro.conf"
 sites_enabled_file = "/etc/nginx/sites-enabled/swin-"+branch_name+"devam.pro.conf"
 project_dir = "/var/www/swin-"+branch_name+".devam.pro"
-domain = "swin-"+branch_name+".devam.pro""
+domain = "swin-"+branch_name+".devam.pro."
 
-response = client.change_resource_record_sets(
+
+os.delete(sites_available_file)
+os.delete(sites_enabled_file)
+shutil.rmtree(project_dir)
+
+deleter53record = client.change_resource_record_sets(
     HostedZoneId='Z3I2KNYQZHGO1Y',
     ChangeBatch= {
         'Changes': [
@@ -20,4 +25,5 @@ response = client.change_resource_record_sets(
                     'ResourceRecords': [{'Value': srv_ip}]
                 }
             }]
-    })
+    }
+)
