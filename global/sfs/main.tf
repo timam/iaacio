641e0e6d004746ee0fb12935b5711b01fc75041c
Timam <me@timam.io>
2018-08-26 09:39:00 +0600

    var implemented for bucket name

--- global/sfs/main.tf
+++ global/sfs/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_s3_bucket" "static-storage" {
-  bucket = "static-storage-timamio"
+resource "aws_s3_bucket" "static-storage-bucket" {
+  bucket = "${var.static-storage-bucket}"
 }
 
--- global/sfs/variables.tf
+++ global/sfs/variables.tf
@@ -2,3 +2,7 @@ variable "aws_region" {
   description = "Region for the VPC"
   default = "us-east-1"
 }
+
+variable "static-storage-bucket" {
+  default = "static-storage-timamio"
+}
