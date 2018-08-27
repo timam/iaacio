b4c581eb052770b2f1c0b7a1ecfe300be12e9661
Timam <me@timam.io>
2018-08-27 14:42:16 +0600

    policy added

--- global/sfs/main.tf
+++ global/sfs/main.tf
@@ -6,3 +6,21 @@ resource "aws_s3_bucket" "static-storage-bucket" {
   bucket = "${var.static-storage-bucket}"
 }
 
+resource "aws_s3_bucket_policy" "static-storage-bucket-policy" {
+  bucket = "${aws_s3_bucket.static-storage-bucket.id}"
+  policy =<<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::static-storage-timamio/*"]
+    }
+  ]
+}
+POLICY
+
+}
